575e930ff5215f2df12bad03f7d1f698ea4d7a7e
TheRealNovelist <novelistoftheyear@gmail.com>
2022-12-01 11:01:43 +0700

    work

--- VR Project/Assets/_Project/_Scripts/Audio/AudioTable.cs
+++ VR Project/Assets/_Project/_Scripts/Audio/AudioTable.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Audio;
 
+
+[System.Serializable] 
 public struct AudioFile
 {
     public string name;
+    
+    [BoxGroup("Settings")]
     [Range(0f, 1f)] public float volume;
-    public float pitch;
-    public List<AudioClip> clips;
+    [BoxGroup("Settings")]
+    [Range(-3f, 3f)] public float pitch;
+    
+    [HideLabel] public WeightedRandomList<AudioClip> clips;
 
-    public AudioFile(string name)
+    public AudioFile(string name, WeightedRandomList<AudioClip> clips)
     {
         this.name = name;
         volume = 1f;
         pitch = 1f;
-        clips = null;
+        this.clips = clips;
     }
 }
 
-public class AudioTable : ScriptableObject
+[CreateAssetMenu(menuName = "System/AudioTable", fileName = "New Audio Table")]
+public class AudioTable : SerializedScriptableObject
 {
     [SerializeField] private AudioMixerGroup _mixerGroup;
-    
-    
+
+    [SerializeField, Searchable] private List<AudioFile> _files;
 }
