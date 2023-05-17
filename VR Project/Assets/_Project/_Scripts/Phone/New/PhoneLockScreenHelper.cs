c6e3eaa330c0c1be4355b73e3f5e2e72902c2114
TheRealNovelist <novelistoftheyear@gmail.com>
2023-05-18 00:44:28 +0700

    Complete Refactor Phone

--- VR Project/Assets/_Project/_Scripts/Phone/New/PhoneLockScreenHelper.cs
+++ VR Project/Assets/_Project/_Scripts/Phone/New/PhoneLockScreenHelper.cs
@@ -10,29 +10,14 @@ using UnityEngine.UI;
 public class PhoneLockScreen : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField] private Phone phone;
-    [SerializeField] private CanvasGroup canvas;
-    [SerializeField] private float fadeDuration;
-    
+
     private void Awake()
     {
         if (!phone) phone = GetComponentInParent<Phone>(true);
     }
 
-    public void TurnOn()
-    {
-        phone.TurnOn();
-        canvas.DOFade(0, fadeDuration).OnComplete(() => gameObject.SetActive(false));
-    }
-
-    [Button]
-    public void TurnOff()
-    {
-        gameObject.SetActive(true);
-        canvas.DOFade(1, fadeDuration).OnComplete(() => phone.TurnOff());
-    }
-    
     public void OnPointerDown(PointerEventData eventData)
     {
-        TurnOn();
+        phone.TurnOn();
     }
 }
